73ab9c83694e42a60a5015030365f90958e9a08f
cathciyal <aruna.cathciyal24@gmail.com>
2022-12-14 19:48:20 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [colour,setColor]=useState("");
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 style={{textAlign:"center"}}>Color Changer</h1>
+      <select value={colour} onChange={e=> setColor(e.target.value)} style={{backgroundColor:colour,width:"300px",height:"30px"}}>
+         <option value="red">Red</option>
+         <option value="yellow" >Yellow</option>
+        <option value="green">green</option>
+      </select>
+      <div style={{height:"200px",width:"590px",backgroundColor:colour,marginLeft:"450px",marginTop:"80px",border:"2px solid black",borderRadius:"7px"}}>
+
+      </div>
     </div>
+   
   );
+
+ 
 }
 
 export default App;
